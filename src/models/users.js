b7b901daf495f9565d660476f9dd20c7f6839c4f
Ayu Istikomah <ayuisti29@gmail.com>
2023-06-13 14:15:45 +0700

    API login register

---
+++ src/models/users.js
@@ -0,0 +1,35 @@
+const dbpool = require("../config/db_animal");
+
+const getAllUsers = () => {
+  const SQLQuery = "SELECT * FROM users";
+
+  return dbpool.execute(SQLQuery);
+};
+
+const createNewUser = (body) => {
+  const SQLQuery = `  INSERT INTO users (userName, email, password) 
+                        VALUES ('${body.userName}', '${body.email}', '${body.password}')`;
+
+  return dbpool.execute(SQLQuery);
+};
+
+const updateUser = (body, idUser) => {
+  const SQLQuery = `  UPDATE users 
+                        SET userName='${body.userName}', email='${body.email}', password='${body.password}' 
+                        WHERE idUser=${idUser}`;
+
+  return dbpool.execute(SQLQuery);
+};
+
+const deleteUser = (idUser) => {
+  const SQLQuery = `DELETE FROM users WHERE idUser=${idUser}`;
+
+  return dbpool.execute(SQLQuery);
+};
+
+module.exports = {
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
+};
