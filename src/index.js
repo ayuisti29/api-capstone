9e72182c135fde725dd5daa4f5d145901baee694
Ayu Istikomah <ayuisti29@gmail.com>
2023-06-13 19:29:06 +0700

    tambah data output

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const usersRoutes = require("./routes/users.js");
 const loginsRoutes = require("./routes/logins.js");
+const outputsRoutes = require("./routes/outputs.js");
 
 const middlewareLogRequest = require("./middleware/logs.js");
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/users", usersRoutes);
 app.use("/logins", loginsRoutes);
+app.use("/outputs", outputsRoutes);
 
 app.get("/beranda", (req, res) => {
   res.send("Selamat Datang di Aplikasi Animal_Recognition");
