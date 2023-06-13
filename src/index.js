be1a08db3541ceed13f5afd5afcb16fc991976b9
Ayu Istikomah <ayuisti29@gmail.com>
2023-06-13 20:31:00 +0700

    upload foto

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const loginsRoutes = require("./routes/logins.js");
 const outputsRoutes = require("./routes/outputs.js");
 
 const middlewareLogRequest = require("./middleware/logs.js");
+const upload = require("./middleware/multer");
 
 const app = express();
 
@@ -16,6 +17,17 @@ app.use(express.json());
 app.use("/users", usersRoutes);
 app.use("/logins", loginsRoutes);
 app.use("/outputs", outputsRoutes);
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.json({
+    message: "upload berhasil",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.json({
+    message: err.message,
+  });
+});
 
 app.get("/beranda", (req, res) => {
   res.send("Selamat Datang di Aplikasi Animal_Recognition");
